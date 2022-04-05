7eb4dfe63fcf67d0c0c162d0c9e229ce1ebd0a94
daaviid9 <davidjancus2@gmail.com>
2022-04-05 11:14:33 +0200

    Initial commit

---
+++ app/src/main/java/sk/upjs/ics/android/tetris/MainActivity.java
@@ -0,0 +1,14 @@
+package sk.upjs.ics.android.tetris;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
