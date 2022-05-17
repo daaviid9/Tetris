270cf95dfc777c08358b6a277af94ca126c7f96a
daaviid9 <davidjancus2@gmail.com>
2022-05-17 12:19:35 +0200

    Music sound switches

--- app/src/main/java/sk/upjs/ics/android/tetris/PopActivity.java
+++ app/src/main/java/sk/upjs/ics/android/tetris/PopActivity.java
@@ -3,7 +3,9 @@ package sk.upjs.ics.android.tetris;
 import static sk.upjs.ics.android.tetris.MenuActivity.option;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.WindowManager;
@@ -18,23 +20,57 @@ public class PopActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop_settings);
+
 
         soundSwitch = (Switch) findViewById(R.id.sound_switch);
         musicSwitch = (Switch) findViewById(R.id.music_switch);
 
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
         soundSwitch = new Switch(this);
+            soundSwitch.setChecked(false);
         soundSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                
+                if (soundSwitch.isChecked()){
+                    prefs.edit().putBoolean("sound", true).commit();
+                }
+                else {
+                    prefs.edit().putBoolean("sound", false).commit();
+                }
+            }
+        });
+
+        musicSwitch = new Switch(this);
+        musicSwitch.setChecked(false);
+        /*
+        if (prefs.getBoolean("music",true)){
+            musicSwitch.setChecked(true);
+            System.out.println("OMG");
+        }
+        else musicSwitch.setChecked(false);
+
+         */
+        musicSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                System.out.println("CHANGE");
+                System.out.println("CHANGE");
+                if (musicSwitch.isChecked()){
+                    prefs.edit().putBoolean("music", true).commit();
+                    System.out.println("MUSIC ON");
+                    System.out.println("MUSIC ON");
+                }
+                else {
+                    prefs.edit().putBoolean("music", false).commit();
+                    System.out.println("MUSIC OFF");
+                    System.out.println("MUSIC OFF");
+                }
             }
         });
 
 
-        if (option == 1)
-            setContentView(R.layout.activity_pop_help);
-        else if (option == 2)
-            setContentView(R.layout.activity_pop_settings);
 
 
         DisplayMetrics dm = new DisplayMetrics();
