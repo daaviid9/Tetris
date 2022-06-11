0561a9253125040af4dd9c9797cf15c93fbbfefb
daaviid9 <davidjancus2@gmail.com>
2022-06-11 12:33:35 +0200

    Pop Register

--- app/src/main/java/sk/upjs/ics/android/tetris/PopLogIn.java
+++ app/src/main/java/sk/upjs/ics/android/tetris/PopLogIn.java
@@ -2,10 +2,15 @@ package sk.upjs.ics.android.tetris;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class PopLogIn extends AppCompatActivity {
 
@@ -13,6 +18,7 @@ public class PopLogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pop_log_in);
+
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -28,5 +34,15 @@ public class PopLogIn extends AppCompatActivity {
         params.y = -20;
 
         getWindow().setAttributes(params);
+
+
+    }
+
+    public void goToRegister(View v){
+        setContentView(R.layout.activity_pop_register);
+    }
+
+    public void backToLogIn(View v){
+        setContentView(R.layout.activity_pop_log_in);
     }
 }
