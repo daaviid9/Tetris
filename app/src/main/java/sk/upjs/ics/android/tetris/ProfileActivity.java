820d6ffd12cf346bb940529601d7fbe9d99e0ef8
poggersmartin <martin.stefanko@gmail.com>
2022-06-13 18:02:57 +0200

    Vypis

--- app/src/main/java/sk/upjs/ics/android/tetris/ProfileActivity.java
+++ app/src/main/java/sk/upjs/ics/android/tetris/ProfileActivity.java
@@ -1,5 +1,6 @@
 package sk.upjs.ics.android.tetris;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,11 +11,24 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
     private Button logout;
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,11 +48,34 @@ public class ProfileActivity extends AppCompatActivity {
 
         getWindow().setAttributes(params);
         logout = (Button) findViewById(R.id.signOut);
+        user= FirebaseAuth.getInstance().getCurrentUser();
+        reference= FirebaseDatabase.getInstance().getReference("Users");
+        userID= user.getUid();
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FirebaseAuth.getInstance().signOut();
                 finish();
+            }
+        });
+
+        final TextView banner= (TextView) findViewById(R.id.bannerDescription);
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userprofile= snapshot.getValue(User.class);
+
+                if (userprofile!=null){
+                    String nick= userprofile.meno;
+
+                    banner.setText("Welcome, "+ nick +"!");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(ProfileActivity.this, "Something wrong happened!", Toast.LENGTH_LONG).show();
             }
         });
     }
